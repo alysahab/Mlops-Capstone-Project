a2a9e6a08712d54e97a94b9fe684890c04d24533
Aly <adnanali@gmail.com>
2025-11-15 11:30:33 +0500

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is capstone MLOPS project using industry standard practices.',
+    author='Adnan Ali',
+    license='MIT',
+)
