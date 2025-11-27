7c06f1a967da3cf7fa644ba01f8254a0ce793ba8
Aly <adnanali@gmail.com>
2025-11-27 11:33:47 +0500

    CICD test-7

--- flask_app/app.py
+++ flask_app/app.py
@@ -10,64 +10,63 @@ from nltk.corpus import stopwords
 import string
 import re
 import dagshub
-from preprocessing_utility import preprocess_text
 
 
 import warnings
 warnings.simplefilter("ignore", UserWarning)
 warnings.filterwarnings("ignore")
 
-# def lemmatization(text):
-#     """Lemmatize the text."""
-#     lemmatizer = WordNetLemmatizer()
-#     text = text.split()
-#     text = [lemmatizer.lemmatize(word) for word in text]
-#     return " ".join(text)
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
 
-# def remove_stop_words(text):
-#     """Remove stop words from the text."""
-#     stop_words = set(stopwords.words("english"))
-#     text = [word for word in str(text).split() if word not in stop_words]
-#     return " ".join(text)
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
 
-# def removing_numbers(text):
-#     """Remove numbers from the text."""
-#     text = ''.join([char for char in text if not char.isdigit()])
-#     return text
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
 
-# def lower_case(text):
-#     """Convert text to lower case."""
-#     text = text.split()
-#     text = [word.lower() for word in text]
-#     return " ".join(text)
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
 
-# def removing_punctuations(text):
-#     """Remove punctuations from the text."""
-#     text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
-#     text = text.replace('؛', "")
-#     text = re.sub('\s+', ' ', text).strip()
-#     return text
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub('\s+', ' ', text).strip()
+    return text
 
-# def removing_urls(text):
-#     """Remove URLs from the text."""
-#     url_pattern = re.compile(r'https?://\S+|www\.\S+')
-#     return url_pattern.sub(r'', text)
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
 
-# def remove_small_sentences(df):
-#     """Remove sentences with less than 3 words."""
-#     for i in range(len(df)):
-#         if len(df.text.iloc[i].split()) < 3:
-#             df.text.iloc[i] = np.nan
+def remove_small_sentences(df):
+    """Remove sentences with less than 3 words."""
+    for i in range(len(df)):
+        if len(df.text.iloc[i].split()) < 3:
+            df.text.iloc[i] = np.nan
 
-# def normalize_text(text):
-#     text = lower_case(text)
-#     text = remove_stop_words(text)
-#     text = removing_numbers(text)
-#     text = removing_punctuations(text)
-#     text = removing_urls(text)
-#     text = lemmatization(text)
+def normalize_text(text):
+    text = lower_case(text)
+    text = remove_stop_words(text)
+    text = removing_numbers(text)
+    text = removing_punctuations(text)
+    text = removing_urls(text)
+    text = lemmatization(text)
 
-#     return text
+    return text
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
@@ -144,7 +143,7 @@ def predict():
 
     text = request.form["text"]
     # Clean text
-    text = preprocess_text(text)
+    text = normalize_text(text)
     # Convert to features
     features = vectorizer.transform([text])
     features_df = pd.DataFrame(features.toarray(), columns=[str(i) for i in range(features.shape[1])])
